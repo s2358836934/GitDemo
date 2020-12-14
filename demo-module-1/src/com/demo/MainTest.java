8d346f859903c86eb7c3d96b1ac94d230ab3ac14
星星眼 <1357181360@qq.com>
2020-12-14 21:29:38 +0800

    尝试进行第5次更改 修改Hello master

--- demo-module-1/src/com/demo/MainTest.java
+++ demo-module-1/src/com/demo/MainTest.java
@@ -5,6 +5,6 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("Hello java");
 
-        System.out.println("Hello Test");
+        System.out.println("Hello master");
     }
 }
