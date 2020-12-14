0c0be73d8a10af56da62f4a13fa0951eb91b407c
星星眼 <1357181360@qq.com>
2020-12-14 21:43:44 +0800

    master分支 修改hello Test

--- demo-module-1/src/com/demo/MainTest.java
+++ demo-module-1/src/com/demo/MainTest.java
@@ -5,6 +5,6 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("Hello java");
 
-        System.out.println("Hello master");
+        System.out.println("hello master");
     }
 }
